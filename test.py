06f383ab025cc794db17674c87184bce74b701c0
Jorge Leites <jorge.l4998@gmail.com>
2018-09-12 08:39:59 -0300

    making the shape prettier

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 
-x = [i/10 for i in range(-20, 20)]
+x = [i/100 for i in range(-500, 500)]
 
-y = [i/10 for i in range(-20, 20)]
+y = [i/100 for i in range(-500, 500)]
 
-z = [[i*j for j in y] for i in x]
+z = [[0 for j in y] for i in x]
+
+i = 0
+
+for u in x:
+    j = 0
+    for v in y:
+        if (u*u + v*v <= 9):
+            z[i][j] = u*v
+        else:
+            z[i][j] = 'nan'
+        j += 1
+    i += 1
 
 data = [
     go.Surface(x=x, y=y, z=z)
